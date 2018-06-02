962f875b4dfcc537e1a86759ca755a9939ad78d6
wixregiga <kecyojagi@protonmail.com>
2018-06-02 09:26:00 -0500

    wrote tests and I am able to get all ppt words into a text file

--- venv/ganalyze/analyzer.py
+++ venv/ganalyze/analyzer.py
@@ -1,43 +1,72 @@
 from pptx import Presentation
 
+
 class Analyzer:
-    '''
+    """
         I want to pass in a path to the powerpoint
         Analyzer analyzes the powerpoint and spits out
         an Analysis.
-        '''
+    """
 
     def __init__(self, ppt_path):
         self.ppt_path = ppt_path
         self.presentation = Presentation(ppt_path)
         self.analysis = None
+        self.title_slides = {}
+        self.list_titles = []
+        self.list_slides = []
+
+        # Call the title slides function
+        self.get_title_slides()
 
     def get_titles(self):
-        titles = {}
+        for k, v in self.title_slides.items():
+            self.list_titles.append(k)
+        return self.list_titles
 
+    def get_slides(self):
+        for idx, slide in enumerate(self.presentation.slides):
+            self.list_slides.append(slide)
+        return self.list_slides
+
+    def get_slide(self, id):
+        return self.list_slides[id]
+
+    def get_title_slides(self):
         for idx, slide in enumerate(self.presentation.slides):
             title_placeholder = slide.shapes.title
-            if (title_placeholder is not None):
+            # self.list_slides.append(slide.slide_id)  # adds all slides to list_slides
+            if title_placeholder is not None:
                 title = title_placeholder.text
-                if title not in titles.keys():
-                    #titles[title].append(slide.slide_id)
-                    titles[title] = [slide.slide_id]
-                elif (slide.slide_id not in titles.get(title)):
-                    titles[title].append(slide.slide_id)
+                if title not in self.title_slides.keys():
+                    self.title_slides[title] = [slide.slide_id]
+                    self.list_titles.append(title)  # adds the title to the list_titles attribute
+                elif slide.slide_id not in self.title_slides.get(title):
+                    self.title_slides[title].append(slide.slide_id)
+        return self.title_slides
 
-        return titles
+    def get_slide_ids(self):
+        ids = []
+        for slide in self.presentation.slides:
+            ids.append(slide.slide_id)
+        return ids
 
-class Analysis:
-    '''
-        ANALYSIS
+    def get_slides_by_title(self, title):
+        slides_by_title = []
+        val = self.title_slides[title]
+        if val is None:
+            return
+        else:
+            for sID in val:
+                slides_by_title.append(sID)
+            return slides_by_title
 
-        - power_point_path:
-            The path to the powerpoint
-        - slides:
-            number of slides in the powerpoint mapped to id
-        - titles: (Dictionary of title keys & slide_id numbers
-            has every title in the powerpoint
-        '''
 
-    def __init__(self):
-        self.power_point_path = ''
+if __name__ == '__main__':
+    a = Analyzer('C:/Users/FlamingSword/Desktop/Python.pptx')
+    a.get_title_slides()
+    list_title_string = ''
+    for title in a.list_titles:
+        print(title)
+        # list_title_string += '{title}\n'.format(title)
+    # print(list_title_string)
--- venv/gnotes.py
+++ venv/gnotes.py
@@ -1,23 +1,79 @@
-from pptx import Presentation
 from ganalyze.analyzer import Analyzer
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# print(dir_path)
-
-# presentation_path = 'C‪:\\Users\\FlamingSword\\Downloads\\Schoolhouse\\Block 03 - Scripting\\Slides\\Python.pptx'
-# prs = Presentation(presentation_path)
-
 
 presentation_path = 'C:/Users/FlamingSword/Desktop/Python.pptx'
+gnote_output_path = 'C:/Users/FlamingSword/Desktop/gnote_output.txt'
+
 my_analyzer = Analyzer(presentation_path)
 
-titles = my_analyzer.get_titles()
 
-for key in titles:
-   print('TITLE: {},   SLIDES:   {}'.format(key, str(titles[key])))
+def test_title_slides(analyzer):
+    titles = analyzer.get_title_slides()
+    sholder = ''
+    for key in titles:
+        sholder += 'TITLE: {},   SLIDES:   {}\n'.format(key, str(titles[key]))
+    return sholder
+
+
+def test_get_all_slide_ids(analyzer):
+    return str(analyzer.get_slide_ids())
+
+
+def test_get_all_slides(analyzer):
+    return str(analyzer.get_slides())
+
+
+def test_get_slides_by_title(analyzer):
+    slides_by_title = analyzer.get_slides_by_title('Mathematical Operations')
+    sholder = ''
+    for slide in slides_by_title:
+        sholder += '{} \n'.format(str(slide))
+    return sholder
+
+
+def test_get_slide(analyzer, id):
+    return str(analyzer.slide(id))
+
+
+def iprint(mfunc, analyzer):
+    name = mfunc.__name__.upper()
+
+    print('\n++++++++++++++++++++++ NOW RUNNING TEST: {}      \n\n'.format(name))
+
+    print(mfunc(analyzer))
+
+    print('\n----------------------- FINISHED RUNNING TEST: {} \n\n\n'.format(name))
+
+def write_ppt_to_file():
+    text_runs = []
+    gnote_output = open(gnote_output_path, 'w')
+
+    for slide in my_analyzer.presentation.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for paragraph in shape.text_frame.paragraphs:
+                for run in paragraph.runs:
+                    text_runs.append(run.text)
+    for text in text_runs:
+        # print('{}\n'.format(text))
+        gnote_output.write('{}\n'.format(text))
+
+    gnote_output.close()
+
+
+if __name__ == '__main__':
+    # iprint(test_title_slides, my_analyzer)
+    # iprint(test_get_all_slides, my_analyzer)
+    # iprint(test_get_all_slide_ids, my_analyzer)
+    # iprint(test_get_slides_by_title, my_analyzer)
+    write_ppt_to_file()
+
+
+
+
 
 # for k, v in enumerate(titles.items()):
 #     print('TITLE: {},   SLIDES:   {}'.format(k, str(v)))
-
 
 
 # prs = Presentation(presentation_path)
